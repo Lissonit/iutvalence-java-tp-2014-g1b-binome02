1c55472f68ab3808530fea59ccef30e5816ad111
lissonit <lissonit@PC-B101-04.iut-valence.fr>
2014-02-19 16:42:27 +0100

    Création de la classe Plateau Insertion des attributs de mesure du Plateau

---
+++ src/fr/iutvalence/java/tp/carunlocker/Plateau.java
@@ -0,0 +1,14 @@
+package fr.iutvalence.java.tp.carunlocker;
+
+public class Plateau
+{
+	/**
+	 * Entier représentant la largeur du plateau de jeu comportant les voitures
+	 */
+	private final int LARGEUR_PLATEAU_DEFAULT = 6;
+	
+	/**
+	 * Entier représentant la hauteur du plateau de jeu comportant les voitures
+	 */
+	private final int HAUTEUR_PLATEAU_DEFAULT = 6;
+}
