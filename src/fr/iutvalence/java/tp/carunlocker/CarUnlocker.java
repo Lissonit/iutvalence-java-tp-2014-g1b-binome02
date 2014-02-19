a042db4bfc21528f94a03489cea52e3121cf1205
lissonit <lissonit@PC-B101-04.iut-valence.fr>
2014-02-19 16:01:24 +0100

    Création de la méthode 'jouer' précédé de sa javadoc

--- src/fr/iutvalence/java/tp/carunlocker/CarUnlocker.java
+++ src/fr/iutvalence/java/tp/carunlocker/CarUnlocker.java
@@ -2,5 +2,11 @@ package fr.iutvalence.java.tp.carunlocker;
 
 public class CarUnlocker
 {
-
+	/**
+	 * Methode de lancement d'une partie
+	 */
+	public void jouer()
+	{
+		// Quelque chose ...
+	}
 }
