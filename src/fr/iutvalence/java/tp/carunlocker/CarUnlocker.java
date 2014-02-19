24a1ac3c432f8bfa972ba2f3a889c357e503a75c
lissonit <lissonit@PC-B101-04.iut-valence.fr>
2014-02-19 16:12:56 +0100

    Création du constructeur précédé de sa javadoc

--- src/fr/iutvalence/java/tp/carunlocker/CarUnlocker.java
+++ src/fr/iutvalence/java/tp/carunlocker/CarUnlocker.java
@@ -2,6 +2,18 @@ package fr.iutvalence.java.tp.carunlocker;
 
 public class CarUnlocker
 {
+	
+	/**
+	 * Retourne une nouvelle partie de jeu
+	 * La voiture principale et les secondaires
+	 * sont disposées dans leurs positions initiales
+	 * Avec le niveau de difficulté 0
+	 */
+	public CarUnlocker()
+	{
+		// Quelque chose ...
+	}
+	
 	/**
 	 * Methode de lancement d'une partie
 	 */
