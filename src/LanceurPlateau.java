d3000e63ff5bf1d0fb4bcd16544154ec571adb6f
lissonit <lissonit@PC-B101-04.iut-valence.fr>
2014-02-19 15:53:14 +0100

    Création du main

---
+++ src/LanceurPlateau.java
@@ -0,0 +1,12 @@
+
+public class LanceurPlateau
+{
+	public static void main(String[] args)
+	{
+		System.out.println("création de le partie");
+		CarUnlocker partieDeCarUnlocker = new CarUnlocker();
+		System.out.println("démarage de le partie");
+		partieDeCarUnlocker.jouer();
+		System.out.println("fin de le partie");
+	}
+}
