8402a57725fdcaf130f4a5538260dced1cd9ecdb
lissonit <lissonit@PC-B101-04.iut-valence.fr>
2014-03-12 16:55:02 +0100

    Suppression du constructeur vide. Création du constructeur avec en paramètre la liste de voiture.

--- src/fr/iutvalence/java/tp/carunlocker/CarUnlocker.java
+++ src/fr/iutvalence/java/tp/carunlocker/CarUnlocker.java
@@ -7,30 +7,22 @@ package fr.iutvalence.java.tp.carunlocker;
  */
 public class CarUnlocker
 {
-	// TODO (fait) écrire un commentaire plus concis
-	/**
-	 * Le niveau de difficulté de la partie
-	 */
-	private int niveau;
-	
 	
 	/**
 	 * Déclaration du nouveau plateau de jeu correspondant au plateau de la nouvelle partie
 	 */
 	private Plateau base;
-	
+		
 	/**
 	 * Retourne une nouvelle partie de jeu
 	 * La voiture principale et les secondaires
 	 * sont disposées dans leurs positions initiales
-	 * Avec le niveau de difficulté 0
+	 * @param listeVoiture liste contenant les voitures à disposer
 	 */
-	public CarUnlocker()
+	public CarUnlocker(Voiture[] listeVoiture)
 	{
-		// Initialisation du niveau de la nouvelle partie
-		this.niveau = 0;
 		// Initialisation du nouveau plateau de jeu correspondant au plateau de la nouvelle partie
-		base = new Plateau();
+		this.base = new Plateau(listeVoiture);
 	}
 	
 	/**
