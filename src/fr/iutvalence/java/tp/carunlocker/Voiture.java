50204ca11229bb90365ad9017dfa98854f71684b
lissonit <lissonit@PC-B101-04.iut-valence.fr>
2014-03-12 16:52:05 +0100

    Création de la classe Voiture Attributs + Constructeur

---
+++ src/fr/iutvalence/java/tp/carunlocker/Voiture.java
@@ -0,0 +1,38 @@
+package fr.iutvalence.java.tp.carunlocker;
+
+
+public class Voiture
+{	
+	/**
+	 * la direction de la voiture
+	 */
+	private Direction.dir direction;
+	
+	/**
+	 * le nombre de case qu'occupe la voiture
+	 */
+	private int taille;
+	
+	/**
+	 * la position de la 'tete' de la voiture
+	 */
+	private Position positionTete;
+	
+	/**
+	 * une nouvelle voiture
+	 * @param positionTete la position de la tete de la voiture
+	 * @param taille la taille de la voiture en nombre de cases
+	 * @param dir la direction de la voiture (horizontal ou vertical)
+	 */
+	public Voiture(Position positionTete, int taille, Direction.dir dir)
+	{
+		this.direction = dir;
+		this.positionTete = positionTete;
+		this.taille = taille;
+	}
+	
+	/*public void DeplacerVers(Direction dir, int nbrCases)
+	{
+		// Déplacement de la voiture de nbrCases avec la direction indiquée
+	}*/
+}
